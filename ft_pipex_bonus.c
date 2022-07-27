5b7b0da95acebe47ab83385f1c53efd63e2c7806
gbertin12 <gaelbertin12@gmail.com>
2022-07-27 22:53:18 +0200

    bug adress cmd

--- ft_pipex_bonus.c
+++ ft_pipex_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 11:58:13 by gbertin           #+#    #+#             */
-/*   Updated: 2022/07/27 15:41:05 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/07/27 22:47:36 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,17 @@
 
 void    ft_print_args(t_list_bonus *cmd)
 {
-    int i = 0;
-    cmd = cmd->next;
-        while (cmd->args[i] != 0)
-        {
-            printf("%d\n", cmd->pid);
-            i++;
-        }
-        //cmd = cmd->next;
-
+    //int i = 0;
+    // while (cmd)
+	// {
+		printf("print : %s\n",cmd->path);
+		// while (cmd->args[i] != 0)
+        // {
+        //     printf("%d\n", cmd->pid);
+        //     i++;
+        // }
+	// 	cmd = cmd->next;
+	// }
 }
 
 int main(int argc, char* argv[], char *envp[])
@@ -38,7 +40,9 @@ int main(int argc, char* argv[], char *envp[])
     cmd = malloc(sizeof(*cmd));
     if (!cmd)
         return (0);
-    ft_init(pipex, cmd, argv, envp);
+    if(!ft_init(pipex, cmd, argv, envp))
+		return (0);
+	printf("after init : %s\n", cmd->path);
     ft_print_args(cmd);
     return (0);
 }
--- src_bonus/init_bonus.c
+++ src_bonus/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: gbertin <gbertin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/26 13:45:30 by gbertin           #+#    #+#             */
-/*   Updated: 2022/07/27 15:40:23 by gbertin          ###   ########.fr       */
+/*   Updated: 2022/07/27 22:48:45 by gbertin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ int ft_init(t_struct *pipex, t_list_bonus *cmd, char *argv[], char *envp[])
 	{
         return (0);
 	}
+	printf("init 2 : %s\n", cmd->path);
 	return (1);
 }
 
@@ -26,14 +27,14 @@ int ft_fill_list(t_struct *pipex, t_list_bonus *cmd, char *argv[])
 {
     int i;
 
-    i = 2;
-    if (!ft_init_list(pipex, cmd, argv[3]))
+    i = 3;
+    if (!ft_init_list(pipex, cmd, argv[2]))
         return (0);
-    while (argv[i] && argv[i + 2])
+    while (argv[i] && argv[i + 1])
     {
 		//check problem
 		//check licks
-		if (ft_addend(pipex, cmd, argv[i]))
+		if (!ft_addend(pipex, cmd, argv[i]))
 			return (0);
 		i++;
     }
@@ -57,17 +58,21 @@ int	ft_addend(t_struct *pipex, t_list_bonus *cmd, char *argv)
 {
 	t_list_bonus *last_cmd;
 	t_list_bonus *new_cmd;
-
+	t_list_bonus *test;
 	new_cmd = malloc(sizeof(t_list_bonus));
 	if (!new_cmd)
 		return (0);
 	last_cmd = ft_islast(cmd);
+	printf("add last : %s\n", last_cmd->path);
 	ft_memset(cmd, 0, sizeof(t_list_bonus));
     new_cmd->pid = 0;
     new_cmd->args = ft_split(argv, ' ');
-	new_cmd->path = ft_get_path_bonus(pipex, cmd);
+	new_cmd->path = ft_get_path_bonus(pipex, new_cmd);
     new_cmd->next = NULL;
 	last_cmd->next = new_cmd;
+
+	test = cmd->next;
+	printf("test : %s\n", test->path);
 	return (1);
 }
 
@@ -77,6 +82,7 @@ int ft_init_list(t_struct *pipex, t_list_bonus *cmd, char *argv)
     cmd->pid = 0;
     cmd->args = ft_split(argv, ' ');
 	cmd->path = ft_get_path_bonus(pipex, cmd);
+	printf("init : %s\n", cmd->path);
     cmd->next = NULL;
 	return (1);
 }
